c9f90227ce5243a398899048930a9e5820bba081
Annarien_Bester <annarien.bester@gmail.com>
2018-12-09 16:16:30 +0200

    Exercise7

--- Exercise7.py
+++ Exercise7.py
@@ -1,19 +1,21 @@
 #Let’s say I give you a list saved in a variable: a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]. 
 # Write one line of Python that takes this list a and makes a new list that has only the even elements of this list in it.
+#import pdb;pdb.set_trace()   #python debugger
+
 
 a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 evenList = []
 oddList = []
-i = 0
+i =0
 
-
-for i in a:
-    modofa = a[i] % 2
-    if (modofa > 0):
+for element in a:
+    if ((a[i]%2) > 0):
         oddList.append(a[i])
     else:
         evenList.append(a[i])
-i=i+1
+    i=i+1
 
 print (evenList)
 print (oddList)
+
+
